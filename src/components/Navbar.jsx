bee4495cf5e30dc122ed85f800e47c73eecc0e0c
Hridoy Chowdhury <t.okay383@gmail.com>
2026-04-13 23:53:03 +0600

    navbar design complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,53 @@
-import React from 'react';
+
+import { ImStatsDots } from 'react-icons/im';
+import { RiHomeGearFill } from 'react-icons/ri';
+import { TbClockCog } from 'react-icons/tb';
+import { NavLink } from 'react-router';
+
 
 const Navbar = () => {
-    return (
-        <div>
-            
-            <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
+
+    const navbarLinks = (
+    <>
+      <li>
+        <NavLink to={'/'} className={({ isActive }) => `${isActive ? "text-white bg-green-500" : ""}`}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={'/timeLine'} className={({ isActive }) => `${isActive ? "text-white bg-green-500" : ""}`}>Listed Books</NavLink>
+      </li>
+      <li>
+        <NavLink to={'/stats'} className={({ isActive }) => `${isActive ? "text-white bg-green-500" : ""}`}>Page to</NavLink>
+      </li>
+    </>
+  )
   
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+    return (
+        <div className='bg-base-200 shadow-sm'>
+
+            <div className="navbar w-11/12 mx-auto flex justify-between ">
+                <div className="">
+                    <a className="text-2xl text-[#005822] font-bold"><span className='text-[#1F2937] '>Keen</span>Keeper</a>
+                </div>
+
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="md:hidden ">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 transition-all duration-300 ease-in-out" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="1"
+                        className="menu menu-sm dropdown-content bg-base-200 rounded-box z-50 mt-8 w-40 p-2 shadow ">
+                        <li><a><RiHomeGearFill/> Home</a></li>
+                        <li><a><TbClockCog /> Timeline</a></li>
+                        <li><a><ImStatsDots /> Stats</a></li>
+                    </ul>
+                </div>
+
+                <div className="hidden md:flex gap-2 items-center ">
+                    <button className="btn"><RiHomeGearFill size={15} />Home</button>
+                    <button className="btn"><TbClockCog size={15} />Timeline</button>
+                    <button className="btn"><ImStatsDots size={15} />Stats</button>
+                </div>
+            </div>
 
         </div>
     );
