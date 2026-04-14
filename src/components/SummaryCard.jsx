77ec31f8aa7b5a6ca7c40764ffc5934ea193144d
Hridoy Chowdhury <t.okay383@gmail.com>
2026-04-14 14:57:10 +0600

    summaryCard design completed

---
+++ src/components/SummaryCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const SummaryCard = () => {
+    return (
+        <>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-6 items-center justify-center text-center px-4'>
+
+                <div className='bg-base-100 p-5 space-y-1 shadow-xl rounded-2xl cursor-pointer transition-all duration-500 hover:scale-105 border border-gray-200 '>
+                    <h2 className='font-bold text-3xl'>10</h2>
+                    <p className='text-gray-500 font-medium'>Total Friends</p>
+                </div>
+                <div className='bg-base-100 p-5 space-y-1 shadow-xl cursor-pointer transition-all duration-500 hover:scale-105 border border-gray-200 rounded-2xl '>
+                    <h2 className='font-bold text-3xl'>3</h2>
+                    <p className='text-gray-500 font-medium'>On Track</p>
+                </div>
+                <div className='bg-base-100 p-5 space-y-1 shadow-xl cursor-pointer transition-all duration-500 hover:scale-105 border border-gray-200 rounded-2xl '>
+                    <h2 className='font-bold text-3xl'>6</h2>
+                    <p className='text-gray-500 font-medium leading-tight'>Need Attention</p>
+                </div>
+                <div className='bg-base-100 p-5 space-y-1 shadow-xl cursor-pointer transition-all duration-500 hover:scale-105 border border-gray-200 rounded-2xl '>
+                    <h2 className='font-bold text-3xl'>12</h2>
+                    <p className='text-gray-500 font-medium leading-tight'>Interactions This Month</p>
+                </div>
+
+            </div>
+            <div className='divider my-8'></div>
+        </>
+    );
+};
+
+export default SummaryCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import HeroBanner from '../components/HeroBanner';
+import SummaryCard from '../components/SummaryCard';
 
 const Home = () => {
     return (
         <div className='w-11/12 mx-auto'>
             <HeroBanner/>
+            <SummaryCard/>
         </div>
     );
 };
